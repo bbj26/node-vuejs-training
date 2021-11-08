41d12956cc991d98844719d26f5e8d2f8638aac8
Branimir <branimir@Branimirs-MacBook-Pro.local>
2021-11-08 16:27:20 +0100

    updated http status codes and messages on failed requests

--- server/controllers/items.js
+++ server/controllers/items.js
@@ -3,9 +3,9 @@ const Item = require('../models/item')
 const fetchItems = async (req, res) => {
   const items = await Item.find({})
   try {
-    res.json(items)
+    res.status(200).json(items)
   } catch (error) {
-    console.log(error)
+    res.status(404).json({code: 404, msg: error})
   }
 }
 
@@ -18,9 +18,9 @@ const createItem = async (req, res) => {
 
   const savedItem = await item.save()
   try {
-    res.status(201).json({ code: 200, message: 'Item added successfully', saved: savedItem})
+    res.status(201).json({ code: 201, message: 'Item added successfully', saved: savedItem})
   } catch (error) {
-    console.log(error)
+    res.status(400).json({code: 400, msg: error})
   }
 }
 
