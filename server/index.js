7a6e64358aefa6a5ca3a1a47cc67a6445d9e8258
bbj26 <brane.corluka@gmail.com>
2021-12-06 11:28:08 +0100

    swagger init

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const app = express();
 const PORT = 4101 || process.env.PORT;
 const router = require('./router');
 require('dotenv').config();
 const cors = require('cors');
+const swaggerUI = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
 
 const mongoose = require('mongoose');
 const db = process.env.DB_CONNECTION;
@@ -11,8 +12,28 @@ mongoose.connect(db)
   .then(console.log('Successfully connected to remote MongoDB.'))
   .catch((err => console.log('Problem with connection to DB. ' + err)));
 
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'To-do API',
+      version: '1.0.0',
+      description: 'To-do app API'
+    },
+    servers: [
+      {
+        url: `http://localhost:${PORT}`
+      }
+    ]
+  },
+  apis: ['./router.js']
+};
+const specs = swaggerJsDoc(options);
+
+const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs));
 app.use('/', router);
 
 app.get('/', (req, res) => {
