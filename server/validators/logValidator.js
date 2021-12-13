69c339e630dba0aca9dd9bdcf6e5fdae7826bc47
bbj26 <brane.corluka@gmail.com>
2021-12-13 14:09:33 +0100

    fixed indents

--- server/validators/logValidator.js
+++ server/validators/logValidator.js
@@ -2,24 +2,24 @@ const { check } = require('express-validator');
 
 const validate = (method) => {
   switch (method) {
-  case 'fetchLogs': {
-    return [
-      check('count', 'Count must be of type Number.').optional()
-        .isNumeric()
-        .not().isString(),
-      check('sort', `Sort must be 'asc' or 'desc'`).optional()
-        .trim()
-        .isIn(['asc', 'desc']),
-      check('level', `Level must be one of: 'fatal', 'error', 'warn', 'info',` +
+    case 'fetchLogs': {
+      return [
+        check('count', 'Count must be of type Number.').optional()
+          .isNumeric()
+          .not().isString(),
+        check('sort', `Sort must be 'asc' or 'desc'`).optional()
+          .trim()
+          .isIn(['asc', 'desc']),
+        check('level', `Level must be one of: 'fatal', 'error', 'warn', 'info',` +
           `'debug', 'trace' `).optional()
-        .trim()
-        .isIn(['fatal', 'error', 'warn', 'info', 'debug', 'trace']),
-      check('message', 'Message must be of type String').optional()
-        .trim()
-        .isString()
-        .not().isNumeric()
-    ];
-  }
+          .trim()
+          .isIn(['fatal', 'error', 'warn', 'info', 'debug', 'trace']),
+        check('message', 'Message must be of type String').optional()
+          .trim()
+          .isString()
+          .not().isNumeric()
+      ];
+    }
   }
 };
 
