6211c11a76b050ada29797c09cd6064aebd04480
bbj26 <brane.corluka@gmail.com>
2021-12-22 09:58:19 +0100

    added reports folder to save pdf-s in; using html-pdf package to convert ejs file to pdf; modified ejs structure to use tables in order to render pdf properly to follow ejs/html look

--- server/controllers/reports.js
+++ server/controllers/reports.js
@@ -1,14 +1,33 @@
 const Employee = require('../models/employee');
 const logger = require('../winston');
 const Task = require('../models/task');
-const { format } = require('date-fns');
+const ejs = require("ejs");
+const pdf = require("html-pdf");
+const path = require("path");
 
 const renderEmployeeReport = async (req, res) => {
   try {
-    const employees = await Employee.find().limit(1);
-    const emp = employees[0];
+    const employees = await Employee.find();
+    const emp = employees[Math.floor(Math.random() * 100)];
     const tasks = await Task.find({ employeeId: emp._id });
-    res.render('report-template', { emp, tasks });
+    //res.render('report-template', { emp, tasks });
+    ejs.renderFile(path.join(__dirname, '../views/', 'report-template.ejs'), { emp, tasks }, (err, data) => {
+      if (err) {
+        res.send(err);
+      } else {
+        let options = {
+          format: 'A4',
+          orientation: 'portrait',
+        };
+        pdf.create(data, options).toFile(path.join(__dirname, '../reports/', `${emp.name}.pdf`), function (err, data) {
+          if (err) {
+            res.send(err);
+          } else {
+            res.send(`<a>${data.filename}</>`);
+          }
+        });
+      }
+    });
   } catch (error) {
     logger.error(`Error rendering employee report. ${error.message}`);
   }
